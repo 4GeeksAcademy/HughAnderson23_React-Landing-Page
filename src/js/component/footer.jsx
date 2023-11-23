bbbf22bf36aad5b01d5e9a0b24580c93828e5225
0xndev <141341761+0xndev@users.noreply.github.com>
2023-11-23 01:33:40 +0000

    jumbotron and footer

---
+++ src/js/component/footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='container-fluid text-center bg-dark text-white p-4 mt-5'>
+      Copyright @ Your Website 2023
+    </div>
+  )
+}
+
+export {Footer}
