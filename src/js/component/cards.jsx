7c6b096a4e5c5ab79f2663e255a213dcbf87168a
Hugh Anderson <144385898+HughAnderson23@users.noreply.github.com>
2023-11-23 01:57:39 +0000

    cards

--- src/js/component/cards.jsx
+++ src/js/component/cards.jsx
@@ -1,23 +1,17 @@
 import React from "react";
 
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+const BootstrapCard = () => {
+	return (
+		<div class="card" style={{width: "18rem"}}>
+		<img src="..." class="card-img-top" alt="..."/>
+		<div class="card-body">
+		  <h5 class="card-title">Card title</h5>
+		  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+		  <a href="#" class="btn btn-primary">Go somewhere</a>
+		</div>
+	  </div>
+	);
+  };
 
-function BasicExample() {
-  return (
-    <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
-  );
-}
-
-export default BasicExample;
+export default BootstrapCard;
 
